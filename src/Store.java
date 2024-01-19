34d0e71310f6f6c2c4f63c2a502234ad2265ca66
hmzshin <hamzasahin@outlook.com>
2024-01-19 14:03:16 +0300

    görev1: Store objesi içerisinde metodlar yazarak diğer objelerin davranışlarını test eder.

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,16 @@
 public class Store {
     public static void main(String[] args) {
+        ProductForSale product1 = new Chocolate("Black", 200, "Best chocolate in the world", 60);
+        ProductForSale product2 = new Coke("Diet", 500, "Without sugar", "Turkish Coke");
+        ProductForSale product3 = new Bread("Brown", 300, "Stay healthy with healthy bread", "Wholewheat");
 
-        listProducts();
+        ProductForSale[] products = {product1, product2, product3};
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+        for (ProductForSale product : products) {
+            product.showDetails();
+        }
     }
 }
