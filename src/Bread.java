565c9afc56f54c527dcb5ed4b8fe2621471d6eb6
hmzshin <hamzasahin@outlook.com>
2024-01-19 14:01:44 +0300

    görev1: Bread objesini ProductForSale objesinden inherit ederek tanımlar.

---
+++ src/Bread.java
@@ -0,0 +1,14 @@
+public class Bread extends ProductForSale {
+    private String kind;
+
+
+    public Bread(String type, long price, String description, String kind) {
+        super(type, price, description);
+        this.kind = kind;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("{" + "price:" + super.getPrice() + "," + "type:" + super.getType() + "," + "description:" + super.getDescription() + "," + "kind:" + this.kind);
+    }
+}
