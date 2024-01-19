0d2e913d25b78949cdab0407eaa475b71450ede5
hmzshin <hamzasahin@outlook.com>
2024-01-19 14:01:01 +0300

    görev1: ProductForSale objesini tanımlar.

---
+++ src/ProductForSale.java
@@ -0,0 +1,31 @@
+public abstract class ProductForSale {
+    private String type;
+    private long price;
+    private String description;
+
+    public ProductForSale(String type, long price, String description) {
+        this.type = type;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getType() {
+
+        return type;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getSalesPrice(int quantity) {
+        return quantity * price;
+    }
+
+    public abstract void showDetails();
+
+}
