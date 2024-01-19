ce4486cbf1d7a794b36838e744eb515facb3ae8c
hmzshin <hamzasahin@outlook.com>
2024-01-19 14:02:08 +0300

    görev1: Chocolate objesini ProductForSale objesinden inherit ederek tanımlar.

---
+++ src/Chocolate.java
@@ -0,0 +1,15 @@
+public class Chocolate extends ProductForSale {
+    private double cacaoPercentage;
+
+    public Chocolate(String type, long price, String description, double cacaoPercentage) {
+        super(type, price, description);
+        this.cacaoPercentage = cacaoPercentage;
+    }
+
+
+    @Override
+    public void showDetails() {
+        System.out.println("{" + "price:" + super.getPrice() + "," + "type:" + super.getType() + "," + "description:" + super.getDescription() + "," + "cacaoPercentage:" + this.cacaoPercentage);
+    }
+
+}
