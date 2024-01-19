96d0b2df8f157677f6cb2a9263343ff0e6977b0d
hmzshin <hamzasahin@outlook.com>
2024-01-19 15:28:36 +0300

    görev2: Yazılan obje ve metodlar test eden metodları içerir.

--- src/Store.java
+++ src/Store.java
@@ -1,16 +1,25 @@
 public class Store {
     public static void main(String[] args) {
-        ProductForSale product1 = new Chocolate("Black", 200, "Best chocolate in the world", 60);
+/*        ProductForSale product1 = new Chocolate("Black", 200, "Best chocolate in the world", 60);
         ProductForSale product2 = new Coke("Diet", 500, "Without sugar", "Turkish Coke");
         ProductForSale product3 = new Bread("Brown", 300, "Stay healthy with healthy bread", "Wholewheat");
+        product2.setPrice(5);
+        System.out.println(product2.getSalesPrice(12));
+        ;
+        System.out.println(product3.getSalesPrice(12));
+        ;
 
         ProductForSale[] products = {product1, product2, product3};
-        listProducts(products);
+        listProducts(products);*/
+
+        Troll troll = new Troll("Troll", 200, 1000);
+        System.out.println(troll.attack());
+
     }
 
-    public static void listProducts(ProductForSale[] products) {
+/*    public static void listProducts(ProductForSale[] products) {
         for (ProductForSale product : products) {
             product.showDetails();
         }
-    }
+    }*/
 }
